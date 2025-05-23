cd26a5a98e8b2780365c6429c728752d2689d9b4
Mpinzon222 <mpinzonh1@ucentral.edu.co>
2025-05-23 17:23:58 -0500

    Modificación de los mensuale

--- app/src/main/java/com/practica/finazapp/Vista/Interfaces/IngresosListener.kt
+++ app/src/main/java/com/practica/finazapp/Vista/Interfaces/IngresosListener.kt
@@ -2,6 +2,7 @@ package com.practica.finazapp.Vista.Interfaces
 
 import com.practica.finazapp.Entidades.IngresoDTO
 
+//Modificar ingresos mensuales
 interface IngresosListener {
 
     fun onIngresosCargados(totalIngresos: Double, ingresosMensuales: List<IngresoDTO>, ingresosCasuales: List<IngresoDTO>)
